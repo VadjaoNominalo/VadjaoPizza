911635685111106935ffd96110a87f640256817f
Vadim <94065790+VadjaoNominalo@users.noreply.github.com>
2022-05-29 13:59:28 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,70 @@
+const langArr = {
+    "intr" : {
+        "ru": "Наши пиццы",
+        "en": "Our pizzas",
+    },
+    "ilja" : {
+        "ru": "Салями Илья",
+        "en": "Salami Ilya",
+    },
+    "salami" : {
+        "ru": "Томатный соус, оливки, салями, сыр",
+        "en": "Tomato sauce, olives, salami, cheese",
+    },
+    "vlad" : {
+        "ru": "BBQ Влад",
+        "en": "BBQ Vlad",
+    },
+    "bbq" : {
+        "ru": "Томатный соус, фрикадельки, сыр, соус барбекю",
+        "en": "Tomato sauce, meatballs, cheese, barbecue sauce",
+    },
+    "martin" : {
+        "ru": "Кебаб с Мартином",
+        "en": "Kebab with Martin",
+    },
+    "kebab" : {
+        "ru": "Томатный соус, лук, халапеньо, кебаб, сыр",
+        "en": "Tomato sauce, onion, jalapeno, kebab, cheese",
+    },
+    "vadik" : {
+        "ru": "Мясной Вадик",
+        "en": "Meat Vadik",
+    },
+    "meet" : {
+        "ru": "Томатный соус, копченая курица, салями, бекон, ветчина, сыр, рваная говядина",
+        "en": "Tomato sauce, smoked chicken, salami, bacon, ham, cheese, pulled beef",
+    },
+    "pablo" : {
+        "ru": "Мексиканский Пабло",
+        "en": "Mexican Pablo",
+    },
+    "mexican" : {
+        "ru": "Чеснок, оливки, халапеньо, пепперони, сыр, соус техас-мекс",
+        "en": "Garlic, olives, jalapeno, pepperoni, cheese, texas-mex sauce",
+    },
+    "vlados" : {
+        "ru": "Влад с зеленью",
+        "en": "Vlad with greenery",
+    },
+    "herbs" : {
+        "ru": "Руккола, бекон, сыр, яйцо, белый соус",
+        "en": "Arugula, bacon, cheese, egg, white sauce",
+    },
+    "4" : {
+        "ru": "Пицца Четыре друга",
+        "en": "Four Friends Pizza",
+    },
+    "tsetire" : {
+        "ru": "Томатный соус, шампиньоны, ананас, паприка, пепперони, ветчина, сыр",
+        "en": "Tomato sauce, mushrooms, pineapple, paprika, pepperoni, ham, cheese",
+    },
+    "news" : {
+        "ru": "Новости",
+        "en": "News",
+    },
+    "news2" : {
+        "ru": "Меню дополняется...",
+        "en": "The menu is complemented...",
+    },
+}
---
+++ js/script.js
@@ -0,0 +1,26 @@
+const select = document.querySelector('select');
+const allLang = ['en', 'ru'];
+
+select.addEventListener('change', changeURLLanguage);
+
+function changeURLLanguage(){
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage(){
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    if (!allLang.includes(hash)){
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    select.value = hash;
+    for (let key in langArr){
+        document.querySelector('.lng-' + key).innerHTML = langArr[key][hash];
+    }
+}
+
+changeLanguage();
+
